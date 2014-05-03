da465354a8fb047b5551be41808c6e0834592996
Augustus Kling <augustus.kling@gmail.com>
2014-05-03 15:33:59 +0200

    Supporting templates for conversion to text.

--- src/main/java/yield/config/function/ToText.java
+++ src/main/java/yield/config/function/ToText.java
@@ -10,6 +10,8 @@ import yield.config.TypedYielder;
 import yield.core.MappedQueue;
 import yield.core.ValueMapper;
 import yield.core.Yielder;
+import yield.json.JsonEvent;
+import yield.json.Template;
 
 public class ToText extends FunctionConfig {
 	@Override
@@ -21,17 +23,39 @@ public class ToText extends FunctionConfig {
 	@Override
 	@Nonnull
 	public TypedYielder getSource(String args, Map<String, TypedYielder> context) {
-		MappedQueue<Object, String> input = new MappedQueue<>(
-				new ValueMapper<Object, String>() {
-
-					@Override
-					public String map(Object value) {
-						return value.toString();
-					}
-				});
-		((Yielder<Object>) context.get(ConfigReader.LAST_SOURCE).yielder)
-				.bind(input);
-		return wrapResultingYielder(input.getQueue());
+		Yielder<? extends Object> yielder;
+		if (args.isEmpty()) {
+			Yielder<Object> input = context.get(ConfigReader.LAST_SOURCE).yielder;
+			MappedQueue<Object, String> mapping = new MappedQueue<>(
+					new ValueMapper<Object, String>() {
+						@Override
+						public String map(Object value) {
+							return value.toString();
+						}
+					});
+			input.bind(mapping);
+			yielder = mapping.getQueue();
+		} else {
+			TypedYielder typedInput = context.get(ConfigReader.LAST_SOURCE);
+			if (typedInput.type.equals(JsonEvent.class.getName())) {
+				final Template template = new Template(args);
+				MappedQueue<JsonEvent, String> mapping = new MappedQueue<>(
+						new ValueMapper<JsonEvent, String>() {
+							@Override
+							public String map(JsonEvent value) {
+								return template.apply(value);
+							}
+						});
+				getYielderTypesafe(JsonEvent.class, ConfigReader.LAST_SOURCE,
+						context).bind(mapping);
+				yielder = mapping.getQueue();
+			} else {
+				throw new IllegalArgumentException("Only "
+						+ JsonEvent.class.getName()
+						+ " is a valid input type when specifiying a template.");
+			}
+		}
+		return wrapResultingYielder(yielder);
 	}
 
 	@Override
