9f0e90b7f62ccdbe188369a1b29b6aac2a6aded3
Augustus Kling <augustus.kling@gmail.com>
2014-08-30 20:56:37 +0200

    Writing to yield.error logger if listener invocation results in an Exception.

--- src/main/java/yield/core/EventSource.java
+++ src/main/java/yield/core/EventSource.java
@@ -3,6 +3,8 @@ package yield.core;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
 import yield.input.FeedingPrevented;
 import yield.input.ListenerExceutionFailed;
 
@@ -19,11 +21,16 @@ public abstract class EventSource<Event> extends BaseControlQueueProvider
 			try {
 				l.feed(logEvent);
 			} catch (ClassCastException e) {
+				Logger.getLogger("yield.error")
+						.warn("Failed to feed an event due to incompatible types.",
+								e);
 				l.getControlQueue().feed(
 						new FeedingPrevented(new IllegalArgumentException(
 								"Due to incompatible types cannot feed to listener "
 										+ l, e)));
 			} catch (Exception e) {
+				Logger.getLogger("yield.error").warn(
+						"Failed to feed an event.", e);
 				l.getControlQueue().feed(
 						new ListenerExceutionFailed<>(logEvent, e));
 			}
